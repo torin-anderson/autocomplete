f5920b4664a86d312a52dbd66c0dd729a0a79ffd
torin-anderson <atorin16@gmail.com>
2024-10-07 20:08:19 -0400

    upload

---
+++ Autocomplete.java
@@ -0,0 +1,16 @@
+public class Autocomplete {
+
+    // Initializes the data structure from the given array of terms.
+    public Autocomplete(Term[] terms)
+
+    // Returns all terms that start with the given prefix,
+    // in descending order of weight.
+    public Term[] allMatches(String prefix)
+
+    // Returns the number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix)
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+}
---
+++ BinarySearchDeluxe.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class BinarySearchDeluxe {
+
+    // Returns the index of the first key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+
+    // Returns the index of the last key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+
+    // unit testing (required)
+    public static void main(String[] args)
+}
