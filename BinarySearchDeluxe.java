8df1dfd78670c45e23a4920e5d621731711e929f
Wally1605 <156972129+Wally1605@users.noreply.github.com>
2024-10-12 01:21:18 -0400

    Update BinarySearchDeluxe.java

--- BinarySearchDeluxe.java
+++ BinarySearchDeluxe.java
@@ -4,12 +4,82 @@ public class BinarySearchDeluxe {
 
     // Returns the index of the first key in the sorted array a[]
     // that is equal to the search key, or -1 if no such key.
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        if (a == null || key == null || comparator == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (comparator.compare(key, a[mid]) < 0) { // less than mid position
+                high = mid - 1;
+            }
+            else if (comparator.compare(key, a[mid]) > 0) { // greater than mid position
+                low = mid + 1;
+            }
+            else { // if key is equal to mid
+
+                if (mid == 0) { // if key is first item
+                    return mid;
+                }
+                else if (comparator.compare(key, a[mid - 1])
+                        > 0) // check if previous key is less than key
+                {
+                    return mid;
+                }
+                else // if previous is equal to or greater than key
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 
     // Returns the index of the last key in the sorted array a[]
     // that is equal to the search key, or -1 if no such key.
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        if (a == null || key == null || comparator == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int low = 0;
+        int high = a.length - 1;
+        int length = a.length - 1;
+
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (comparator.compare(key, a[mid]) < 0) { // less than mid position
+                high = mid - 1;
+            }
+            else if (comparator.compare(key, a[mid]) > 0) { // greater than mid position
+                low = mid + 1;
+            }
+            else { // if key is equal to mid
+                if (mid == length) { // if key is all the way to the right. (last key)
+                    return mid;
+
+                }
+                else if (comparator.compare(key, a[mid + 1]) < 0) // Next position greater?
+                {
+                    return mid;
+                }
+                else // if next item is also equal/less
+                {
+                    low = mid + 1;
+                }
+            }
+        }
+
+
+        return -1;
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+    }
 }
